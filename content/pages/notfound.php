89e9105cc7c41fad5df9448f719cf8995f62fdd3
Kieran Cross <me@kilosierracharlie.me>
2017-05-16 14:23:58 +0100

    Basic Version
    The basic version of the template-based system.

---
+++ content/pages/notfound.php
@@ -0,0 +1,12 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 text-center">
+		    <h1>Error: Page not found.</h1>
+		    <p class="lead">The page you were looking for, <?php echo $errorpage; ?> was not found.</p>
+		    <ul class="list-unstyled">
+		        <li>Bootstrap v3.3.7</li>
+		        <li>jQuery v1.11.1</li>
+		    </ul>
+		</div>
+	</div>
+</div>
